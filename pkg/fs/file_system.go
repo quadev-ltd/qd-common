886454ec24bf3b1992b8eb29ba10cf2fb03251d4
Gustavo Franco <gusfran17@gmail.com>
2024-03-03 17:10:59 -0300

    chore: Add continuous integration pipeline (#1)
    * Add continuuous integration pipeline
    
    * Add linting fixes
    
    * Remove unnecessary set ups
    
    ---------
    
    Co-authored-by: gustavo-m-franco <gusfran17@gmail>

--- pkg/fs/file_system.go
+++ pkg/fs/file_system.go
@@ -4,6 +4,7 @@ import (
 	"os"
 )
 
+// FileSystem is an interface for file system operations
 type FileSystem interface {
 	Create(name string) (*os.File, error)
 	ReadFile(filename string) ([]byte, error)
@@ -15,28 +16,35 @@ type FileSystem interface {
 
 var _ FileSystem = OSFileSystem{}
 
+// OSFileSystem is an implementation of FileSystem
 type OSFileSystem struct{}
 
+// ReadFile reads a file
 func (OSFileSystem) ReadFile(filename string) ([]byte, error) {
 	return os.ReadFile(filename)
 }
 
+// WriteFile writes a file
 func (OSFileSystem) WriteFile(filename string, data []byte, perm os.FileMode) error {
 	return os.WriteFile(filename, data, perm)
 }
 
+// Stat stats a file
 func (OSFileSystem) Stat(name string) (os.FileInfo, error) {
 	return os.Stat(name)
 }
 
+// Mkdir makes a directory
 func (OSFileSystem) Mkdir(name string, perm os.FileMode) error {
 	return os.Mkdir(name, perm)
 }
 
+// IsNotExist checks if an error is a not exist error
 func (OSFileSystem) IsNotExist(err error) bool {
 	return os.IsNotExist(err)
 }
 
+// Create creates a file
 func (OSFileSystem) Create(name string) (*os.File, error) {
 	return os.Create(name)
 }
