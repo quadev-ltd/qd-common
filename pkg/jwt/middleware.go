8ffecfbabf4212ac7c5d68ee60c75c86d775aa17
gustavo-m-franco <gusfran17@gmail>
2024-03-14 11:12:50 +0000

    Add function to get claims

--- pkg/jwt/middleware.go
+++ pkg/jwt/middleware.go
@@ -2,9 +2,27 @@ package jwt
 
 import (
 	"context"
+	"fmt"
+	"time"
 
 	"google.golang.org/grpc/metadata"
+
+	commonToken "github.com/quadev-ltd/qd-common/pkg/token"
 )
+
+type ContextKey string
+
+const (
+	ClaimsContextKey ContextKey = "ContextClaimsKey"
+)
+
+// TokenClaims is the claims for a JWT token
+type TokenClaims struct {
+	Email  string
+	Type   commonToken.TokenType
+	Expiry time.Time
+	UserID string
+}
 
 // AddAuthorizationMetadataToContext adds the authorization Bearer token to the context
 func AddAuthorizationMetadataToContext(ctx context.Context, token string) context.Context {
@@ -17,4 +35,12 @@ func AddAuthorizationMetadataToContext(ctx context.Context, token string) contex
 	})
 	mergedMD := metadata.Join(existingMD, newMD)
 	return metadata.NewOutgoingContext(ctx, mergedMD)
+}
+
+// GetLoggerFromContext returns the logger from the context
+func GetClaimsFromContext(ctx context.Context) (*TokenClaims, error) {
+	if claims, ok := ctx.Value(ClaimsContextKey).(*TokenClaims); ok {
+		return claims, nil
+	}
+	return nil, fmt.Errorf("Claims not found in context")
 }
