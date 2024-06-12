ed39ea9dd7bdae42b5aa3abf0f792a489cb579cc
gustavo-m-franco <gusfran17@gmail>
2024-06-12 19:20:39 +0100

    Add Local, Dev and Prod AWS config IDs

--- pkg/aws/constants.go
+++ pkg/aws/constants.go
@@ -14,18 +14,29 @@ type AppConfig struct {
 	ConfigurationProfileID string
 }
 
-// LocalConfig is the configuration for the development environment
+const ApplicationID = "wwrvnjk"
+const Region = "eu-west-1"
+
+// LocalConfig is the configuration for the local/development environment
 var LocalConfig = AppConfig{
-	Region:                 "eu-west-1",
-	ApplicationID:          "wwrvnjk",
-	EnvironmentID:          "iv285bh",
-	ConfigurationProfileID: "9va7r42",
+	Region:                 Region,
+	ApplicationID:          ApplicationID,
+	EnvironmentID:          "gl16bm7",
+	ConfigurationProfileID: "du1vndf",
 }
 
 // DevConfig is the configuration for the development environment
 var DevConfig = AppConfig{
-	Region:                 "eu-west-1",
-	ApplicationID:          "wwrvnjk",
-	EnvironmentID:          "iv285bh",
-	ConfigurationProfileID: "9va7r42",
+	Region:                 Region,
+	ApplicationID:          ApplicationID,
+	EnvironmentID:          "gl16bm7",
+	ConfigurationProfileID: "du1vndf",
+}
+
+// ProdConfig is the configuration for the production environment
+var ProdConfig = AppConfig{
+	Region:                 Region,
+	ApplicationID:          ApplicationID,
+	EnvironmentID:          "9t9usa0",
+	ConfigurationProfileID: "2yy0ge2",
 }
